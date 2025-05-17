45ed4c0359c06f2a9df71b1a8dd103111a74a320
Diego Herrera <dieg.herrerap@duocuc.cl>
2025-05-17 11:15:03 -0400

    Corrección (borrado) metodos de controller

--- Evento/src/main/java/com/example/Evento/controller/EventoController.java
+++ Evento/src/main/java/com/example/Evento/controller/EventoController.java
@@ -10,37 +10,6 @@ import jakarta.persistence.EntityManager;
 
 public class EventoController {
 
-    @Autowired
-    private EntityManager entityManager;
-
-
-    //buscar todos los eventos
-    public List<Evento> findAllEventos() {
-        String query = "SELECT e FROM Evento e";
-        return entityManager.createQuery(query, Evento.class).getResultList();
-    }
-
-    //buscar evento por id
-    public Evento findEventoById(int id) {
-        return entityManager.find(Evento.class, id);
-    }
-
-    //guardar evento
-    public void saveEvento(Evento evento) {
-        entityManager.persist(evento);
-    }
-
-    //actualizar evento
-    public void updateEvento(Evento evento) {
-        entityManager.merge(evento);
-    }
-
-    //eliminar evento
-    public void deleteEvento(int id) {
-        Evento evento = findEventoById(id);
-        if (evento != null) {
-            entityManager.remove(evento);
-        }
-    }
+ 
 
 }
