98991b4e6b8523a7e39ae78be18e629d5256c91a
Estebanbravo1 <es.bravoa@duocuc.cl>
2025-06-11 21:46:36 -0400

    Ajustes Usuario sin .env

---
+++ Usuario/src/main/java/com/example/Usuario/DataLoader.java
@@ -0,0 +1,36 @@
+package com.example.Usuario;
+
+import com.example.Usuario.model.Usuario;
+import com.example.Usuario.repository.UsuarioRepository;
+import net.datafaker.Faker;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
+
+@Profile("dev")
+@Component
+public class DataLoader implements CommandLineRunner {
+
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
+    @Override
+    public void run(String... args) throws Exception {
+        Faker faker = new Faker();
+
+        for (int i = 0; i < 10; i++) {
+            Usuario usuario = new Usuario();
+            usuario.setNombre(faker.name().fullName());
+            usuario.setEmail(faker.internet().emailAddress());
+            usuario.setContraseña(faker.internet().password());
+            usuario.setFechaRegistro(LocalDate.now());
+            usuario.setDireccion(faker.address().fullAddress());
+            usuario.setTelefono(faker.phoneNumber().phoneNumber());
+            usuarioRepository.save(usuario);
+        }
+    }
+
+}
